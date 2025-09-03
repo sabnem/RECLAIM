d0d5f70ae5952a2002ff0efc7b931b36be0c4340
JsNem74 <146393172+sabnem@users.noreply.github.com>
2025-09-03 17:06:42 +0300

    Update settings.py

--- lost_and_found/settings.py
+++ lost_and_found/settings.py
@@ -77,23 +77,35 @@ WSGI_APPLICATION = 'lost_and_found.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
- #       'ENGINE': 'django.db.backends.postgresql',
-  #      'NAME': 'ReClaimDB',
-   #     'USER': 'postgres',
-    #    'PASSWORD': 'Joyce@2003',
-    #    'HOST': 'localhost',
-    #    'PORT': '5432',
-    #}
-#}
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'ReClaimDB',
+#        'USER': 'postgres',
+#        'PASSWORD': 'Joyce@2003',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'JsNEM$ReclaimDB',
+        'USER': 'JsNEM',
+        'PASSWORD': 'Joyce@2003',
+        'HOST': 'JsNEM.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
