749813aa7a9c7b61387689da329ee2a5a33ddba1
JsNem74 <146393172+sabnem@users.noreply.github.com>
2025-09-03 15:32:40 +0300

    Update settings.py

--- lost_and_found/settings.py
+++ lost_and_found/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@)y*t1^%l$cj0+k-r&$uj!n2x$oc0r4urw((7p$zsde!h+$+yx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['JsNEM.pythonanywhere.com']
+ALLOWED_HOSTS = ['JsNEM.pythonanywhere.com', 'jsnem.pythonanywhere.com']
 
 
 # Application definition
