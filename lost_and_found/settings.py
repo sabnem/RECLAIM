e8b0c9e514556f63bd0dfc621559eb30fa5d5476
JsNem74 <146393172+sabnem@users.noreply.github.com>
2025-09-10 18:56:24 +0300

    Update settings for production deployment
    Set DEBUG to False and restrict ALLOWED_HOSTS to specific domains for production. Switch database configuration from local PostgreSQL to remote MySQL for deployment on PythonAnywhere.

--- lost_and_found/settings.py
+++ lost_and_found/settings.py
@@ -26,9 +26,10 @@ SECRET_KEY = 'django-insecure-@)y*t1^%l$cj0+k-r&$uj!n2x$oc0r4urw((7p$zsde!h+$+yx
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#ALLOWED_HOSTS = ['JsNEM.pythonanywhere.com', 'jsnem.pythonanywhere.com']
-ALLOWED_HOSTS = ['*']
+DEBUG = False
+# DEBUG = True
+ALLOWED_HOSTS = ['JsNEM.pythonanywhere.com', 'jsnem.pythonanywhere.com']
+# ALLOWED_HOSTS = ['*']
 
 
 
@@ -86,28 +87,28 @@ WSGI_APPLICATION = 'lost_and_found.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 # POSTGRES
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'ReClaimDB',
-       'USER': 'postgres',
-       'PASSWORD': 'Joyce@2003',
-       'HOST': 'localhost',
-       'PORT': '5432',
-    }
-}
-
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'JsNEM$ReclaimDB',
-#         'USER': 'JsNEM',
-#         'PASSWORD': 'Joyce@2010',
-#         'HOST': 'JsNEM.mysql.pythonanywhere-services.com',
-#         'PORT': '3306',
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'ReClaimDB',
+#        'USER': 'postgres',
+#        'PASSWORD': 'Joyce@2003',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'JsNEM$ReclaimDB',
+        'USER': 'JsNEM',
+        'PASSWORD': 'Joyce@2010',
+        'HOST': 'JsNEM.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
